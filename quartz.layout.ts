ffed19db198ef6e3eaa9bde8863db3905f16e987
RudenkoBogdan <rudenko.ba03@gmail.com>
2026-04-13 23:04:33 +0300

    #4 style - more academic

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,10 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      Repo: "https://github.com/RudenkoBogdan/nnvault",
+      GitHub: "https://github.com/RudenkoBogdan",
+      Kaggle: "https://www.kaggle.com/rudenkobogdan",
+      LinkedIn: "https://www.linkedin.com/in/bogdanrudenko/",
     },
   }),
 }
